e078fb287c2ae33dca5ab80857139a640a0689a0
Gabriel ML <gabrielmljob@gmail.com>
2025-09-23 14:54:50 +0200

    feat: create base `Footer` component structure.
    - [x] Create `Footer` folder.
    - [x] Create `Footer.jsx` file with basic structure.
    - [x] Create `Footer.css` styles.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    <footer className="footer-container">
+        {/* TODO: SVP Follow Figma Prototype */}
+        <p className="footer-text">&copy; 2025 Happy Paws Veterinary Clinic. All rights reserved.</p>
+    </footer>
+};
+
+export default Footer;
