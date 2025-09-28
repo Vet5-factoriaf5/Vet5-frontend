2cc355ca946544d9d4986ab328ff61f95ca59d5c
Paula Apsé <89433554+Kenhya12@users.noreply.github.com>
2025-09-28 15:40:45 +0200

    Add NavBar login button and Footer with centered logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Home from "./pages/Home";
+import Footer from './components/Footer';
+import Nav from "./components/Nav";
+import './components/footer.css';
 
 function App() {
   return (
     <div>
+      <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/nav.jsx
@@ -0,0 +1,15 @@
+import './Nav.css';
+import navImage from '../assets/images/Logo-Happy-Paws.svg';
+import userIcon from "../assets/images/icons/user dark.svg";
+
+export default function Nav() {
+    return (
+        <nav className="nav">
+            <img src={navImage} alt="Nav" />
+            <button className="btn-login">
+                <img src={userIcon} alt="user icon" className="login-icon" />
+                <span>| Login</span>
+            </button>
+        </nav>
+    );
+}
